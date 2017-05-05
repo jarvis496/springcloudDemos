7339379ab0c2b304fa97a60b2327d94d8c6978d5
jarvis496 <517268938@qq.com>
2017-05-05 09:32:31 +0800

    项目提交

---
+++ src/test/java/cn/cciab/springclouddemo/EruekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cciab.springclouddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EruekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
